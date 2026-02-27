9308467160097f3f9b792201cb5d1b8b5974b33a
SCCGazi <gxskbyl@gmail.com>
2026-02-27 04:27:34 +0000

    adds ball 8

--- bin/magic-8-ball.sh
+++ bin/magic-8-ball.sh
@@ -12,3 +12,12 @@ ANSWERS=(
     "Very doubtful."
     "Ask again later."
 )
+
+echo "--- The Magic 8-Ball ---"
+read -p "Ask the 8-Ball a question and press Enter: "
+echo "Shaking the 8-Ball..."
+
+NUM_ANSWERS=${#ANSWERS[@]}
+RANDOM_INDEX=$(( RANDOM % NUM_ANSWERS ))
+
+echo "8-BALL SAYS: ${ANSWERS[RANDOM_INDEX]}"
