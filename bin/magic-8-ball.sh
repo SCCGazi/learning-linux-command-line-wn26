98e9b9fad424e0056567126b6307fdb681c2daf0
SCCGazi <gxskbyl@gmail.com>
2026-02-27 04:09:50 +0000

    something new

---
+++ bin/magic-8-ball.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# A script that simulates a Magic 8-Ball using an array and randomness.
+
+# Define the array of possible answers (choices are separated by spaces)
+ANSWERS=(
+    "It is certain."
+    "Yes, definitely."
+    "Reply hazy, try again."
+    "Cannot predict now."
+    "Don't count on it."
+    "Outlook not so good."
+    "Very doubtful."
+    "Ask again later."
+)
